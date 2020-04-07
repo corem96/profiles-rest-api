dea5b78daf74891a756349db1a4dff2130a4adaf
Jorge Escamilla <corem96@gmail.com>
2020-04-06 19:38:50 -0600

    feat(api): create post method using serializer to validate data and send response data

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_app import serializers
 
 
 class ProfileApiView(APIView):
@@ -15,3 +17,14 @@ class ProfileApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an api view': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with given name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
