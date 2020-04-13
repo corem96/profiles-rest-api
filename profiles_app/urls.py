46c41288f89e12eee8e551a6cbfac2ca8800dacc
Jorge Escamilla <corem96@gmail.com>
2020-04-12 21:44:54 -0600

    feat: added login API with token generation

--- profiles_app/urls.py
+++ profiles_app/urls.py
@@ -10,5 +10,6 @@ router.register('profiles', views.UserProfileViewSet)
 
 urlpatterns = [
     path('profiles-view/', views.ProfileApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
